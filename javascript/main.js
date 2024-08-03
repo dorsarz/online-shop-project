d1e0a23f5c19dc463a530728fb49293d1fa90f9b
dorsarz <dorsarezaei24@gmail.com>
2024-08-03 14:13:34 +0330

    special offer part added

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,21 @@
 let slideIndex = 1 ;
+let reminderingTime= 70000;
+function setTime(){
+    if(reminderingTime==0) return;
+    let h=Math.floor(reminderingTime/3600);
+    let m =Math.floor(reminderingTime%3600/60);
+    let s=(reminderingTime%3600)%60;
+    document.querySelector('#hours').innerHTML= h;
+    document.querySelector('#minutes').innerHTML= m;
+    document.querySelector('#seconds').innerHTML= s;
+}
+setInterval(()=>{
+reminderingTime -= 1;
+setTime()
+}, 1000)
+
+
+
 function setSlide(input,index){
     slideIndex = index;
     let item = document.querySelector(`#${input}`)
